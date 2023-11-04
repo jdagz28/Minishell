6fa425edb52ca417b043a96b8e02abcd8bc647d2
Joshua Dagoy <joshdagoy28@gmail.com>
2023-11-04 02:00:09 +0100

    token tester needs review

--- srcs/lexer/token_test.c
+++ srcs/lexer/token_test.c
@@ -6,7 +6,7 @@
 /*   By: jdagoy <jdagoy@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 22:21:46 by jdagoy            #+#    #+#             */
-/*   Updated: 2023/11/04 00:24:36 by jdagoy           ###   ########.fr       */
+/*   Updated: 2023/11/04 01:59:29 by jdagoy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,6 @@ const char *error_redirect[] = {
 	"cat > &&",
 	"cat > ||",
 	"cat > ;",
-	"cat",
 	NULL
 };
 
@@ -65,7 +64,7 @@ const char *error_operator[] = {
 	"cat | ||",
 	"cat ||||",
 	"cat | ;",
-	"( && )",
+	// "( || )",
 	"cat || &&",
 	"cat || |",
 	"cat && ;",
@@ -78,21 +77,36 @@ const char *error_word[] = {
 	"echo \\x"
 };
 
+void	free_token(t_token *head);
+
 int main(void)
 {
 	t_token		*tokens;
 	int			i;
 	char		*line;
 	const char		**error_tests[] = {error_invalid, error_redirect, error_operator, error_operator, NULL};
+	const char		*tests[] = {"error_invalid", "error_redirect", "error_operator", "error_operator"};
 
 	for (int i = 0; error_tests[i] != NULL; i++)
 	{
-		printf("%s\n", (char *)error_tests[i]);
+		printf("%s\n", tests[i]);
 		for (int j = 0; error_tests[i][j] != NULL; j++)
 		{
-			printf("Input: %s", (char *)error_tests[i][j]);
+			printf("Input: %s\n", (char *)error_tests[i][j]);
 			tokens = tokenizer((char *)error_tests[i][j]);
-			free(tokens);
+			if (check_tokens(tokens) == false || check_wordtokens(tokens) == false)
+			{
+				free_token(tokens);
+			}
+			while (tokens != NULL && tokens->kind != TK_EOF)
+			{
+				printf("Token %d\n", j);
+				printf("Token: \t%s\n", tokens->word);
+				printf("Kind: \t%s", token_kind_strings[tokens->kind]);
+				tokens = tokens->next;
+			}
+			free_token(tokens);
+			printf("\n");
 		}
 		printf("====================\n\n");
 	}
@@ -101,6 +115,7 @@ int main(void)
 	{
 		i = 0;
 		line = readline("Token_tester > ");
+		// printf("%s\n", line);
 		tokens = tokenizer(line);
 		while (tokens != NULL && tokens->kind != TK_EOF)
 		{
@@ -109,9 +124,28 @@ int main(void)
 			printf("Kind: \t%s", token_kind_strings[tokens->kind]);
 			tokens = tokens->next;
 		}
+		free_token(tokens);
 		free(line);
 	}
-	free(tokens);
 	return (0);
 }
 
+void	free_token(t_token *head)
+{
+	t_token	*itr;
+	t_token	*next;
+
+	if (head == NULL)
+		return ;
+	itr = head;
+	next = itr->next;
+	while (next != NULL)
+	{
+		free(itr->word);
+		free(itr);
+		itr = next;
+		next = itr->next;
+	}
+	free(itr->word);
+	free(itr);
+}
