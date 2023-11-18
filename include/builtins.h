ea70d4fb8192947e48a959c716737141975d6bd7
jdagoy <jdagoy@student.s19.be>
2023-11-18 20:41:03 +0100

    PATH_MAX and NAME_MAX at 19 macs

--- include/builtins.h
+++ include/builtins.h
@@ -6,7 +6,7 @@
 /*   By: jdagoy <jdagoy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 15:15:05 by jdagoy            #+#    #+#             */
-/*   Updated: 2023/11/17 13:20:45 by jdagoy           ###   ########.fr       */
+/*   Updated: 2023/11/18 20:40:40 by jdagoy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@
 define PATH_MAX 4096
 define NAME_MAX 255
 
-//!check PATH_MAX and NAME_MAX at 19
-// # else
-// define PATH_MAX 
-// define NAME_MAX
+//echo "$(getconf {PATH_MAX / NAME_MAX} /)"
+# else
+define PATH_MAX 1024
+define NAME_MAX 255
 
 # endif
 
